14e1e2d33ba5dbe84589d0b27239c059291c2a1d
Vaidehi Bapat <vaidehihb@gmail.com>
2019-01-09 22:20:59 -0500

    Added GCD class

---
+++ src/com/linkedinjava/Main.java
@@ -0,0 +1,10 @@
+package com.linkedinjava;
+import com.linkedinjava.algorithms.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(GCD.gcd(18,24));
+    }
+}
---
+++ src/com/linkedinjava/algorithms/GCD.java
@@ -0,0 +1,13 @@
+package com.linkedinjava.algorithms;
+
+public class GCD {
+    public static int gcd(int a, int b){
+        int temp;
+        while(b!=0){
+            temp = a;
+            a = b;
+            b = temp % b;
+        }
+        return a;
+    }
+}
